5099d68baef601f0b8e2b68e6f9ac1dbdc49c9d2
bogdan_yatsiv <bogdanyatsiv@gmail.com>
2021-09-08 17:47:35 +0300

    add new request

--- CarsBD/Program.cs
+++ CarsBD/Program.cs
@@ -37,15 +37,13 @@ namespace CarsBD
                 Console.ReadKey();
                 Console.WriteLine();
 
-                //var serviceCenters = context.ServiceJobs.Include(navigationPropertyPath: sj => sj.Car.Sales).
-                //    AsEnumerable().GroupBy(sj => sj.ServiceCenter.Name).
-                //    Select(sc => new { 
-                //        Name = sc.Key, 
-                //        Price = sc.Select(s => s.Car.Sales.Max(car => car.Price).Value).First()
-                //    });
-
-
-
+                var centers = context.ServiceJobs.Join(context.Sales, sj => sj.car_id, s => s.car_id,
+                                                    (sj, s) => new { service = sj.ServiceCenter.Name, price = s.Price }).
+                                                    GroupBy(services => services.service).
+                                                    Select(it => new { service = it.Key, price = it.Max(s => s.price) }).
+                                                    OrderByDescending(it => it.price);
+                    
+                
                 var servicePrice = from sj in context.Set<ServiceJob>()
                                    join s in context.Set<Sale>()
                                    on sj.car_id equals s.car_id
@@ -54,33 +52,12 @@ namespace CarsBD
                 var sortedServices = from sc in servicePrice
                                      group sc by sc.Service into g
                                      select new { Name = g.Key, Price = g.Max(s => s.Price) };
-                
-                //foreach(var sc in serviceCenters)
-                //{
-                //    int maxPrice = 0;
-
-                //    foreach(var car in sc)
-                //    {
-                //        var sales = car.Car.Sales.ToList();
-                //        foreach (var s in sales)S
-
-                //        {
-                //            if (s.Price > maxPrice) maxPrice = (int)s.Price;
-                //        }
-                //    }
-                //    servicePrice.Add(sc.Key, maxPrice);
-                //}
 
                 Console.WriteLine("Service centers that serve cars that are sold by most price desc:");
-                foreach(var s in sortedServices.OrderByDescending(ss => ss.Price))
+                foreach(var s in centers)
                 {
-                    Console.WriteLine($"{s.Name} - {s.Price}");
+                    Console.WriteLine($"{s.service} - {s.price}");
                 }
-                //foreach(var sc in serviceCenters)
-                //{
-                //    Console.WriteLine($"{sc.Name} - {sc.Price}");
-                //}
-                
             }
         }
     }
